711518f67b0edf9ad7f66aaba89a878faa19af56
Amanuel Solomon <amanuel.ugr-0540-16@aau.edu.et>
2025-11-10 19:32:28 +0300

    Project Step 1: Setup CRA and Git for Task Manager

---
+++ task-manager/src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home=()=>{
+    return(
+        <div>
+            <h2>Welcome to Task manager</h2>
+            <p>manage your tasks and stay organized!</p>
+            <div style={{marginTop:'20px'}}>
+                <button>Get Started</button>
+            </div>
+        </div>
+    );
+};
+export default Home;
