0130aacb241c23ac00455eefcdffa45a8d3e5435
Amanuel Solomon <amanuel.ugr-0540-16@aau.edu.et>
2025-11-29 23:51:31 +0300

    Back-End Day 2: Added middleware, error handling, CRUD endpoints

--- backend-basics/server.js
+++ backend-basics/server.js
@@ -1,32 +1,59 @@
 const express = require('express');
 const app = express();
 const PORT = 3002;
+
 app.use(express.json());
 
-let tasks = [];
-app.get('/', (req, res)=>{
-    res.send('Welcome to express!');
+app.use((req, res, next)=>{
+    console.log(`${req.method} ${req.url}`);
+    next();
 });
-app.get('/tasks', (req, res)=>{
+app.use((req, res, next)=>{
+    if(req.method === 'POST' && req.url === '/tasks' && !req.body.task){
+        return res.status(400).send('Task is required');
+    }
+    next();
+});
+
+let tasks = [];
+
+app.get('/tasks', (req, res) =>{
     res.json(tasks);
 });
+
 app.post('/tasks', (req, res)=>{
     const newTask = req.body.task;
-    if(!newTask){
-        return res.status(400).json({message: "Task is required"});
-    }
     tasks.push(newTask);
-    res.json({message: 'Task Added', tasks: tasks});        
+    res.json({message: 'Task Added', tasks});
+});
+
+app.put('/tasks/:index', (req, res)=>{
+    const index = parseInt(req.params.index);
+    const newTask = req.body.task;
+    if(index <0 || index >=tasks.length){
+        return res.status(400).send('Task not found');
+    }
+    tasks[index] = newTask;
+    res.json({message: 'Task updated', tasks});
 });
 app.delete('/tasks/:index', (req, res)=>{
     const index = parseInt(req.params.index);
-    if(isNaN(index) || index<0 || index>=tasks.length){
-        return res.status(404).json({message: 'Taks not found'});
-    }
-    const deletedTask = tasks.splice(index, 1);
 
-    res.json({message: 'task deleted successfully', deleted: deletedTask, tasks:tasks});
+    if(index <0 || index>= tasks.length){
+        return res.status(400).send('Task not found');
+    }
+    const deleted =tasks.splice(index, 1);
+    res.json({
+        message: 'Task Deleted',
+        deleted,
+        tasks
+    });
 });
+app.use((err, req, res, next)=>{
+    console.error(err.stack);
+    res.status(500).send('Server error');
+});
+
 app.listen(PORT, ()=>{
-    console.log('server running on http://localhost:${PORT}');
+    console.log(`Server running on http://localhost:${PORT}`);
 });
