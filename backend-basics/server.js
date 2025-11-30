e0366798e64b34a8dda5c21edabdd573945e20c5
Amanuel Solomon <amanuel.ugr-0540-16@aau.edu.et>
2025-11-30 23:36:14 +0300

    middleware for auth stub

--- backend-basics/server.js
+++ backend-basics/server.js
@@ -5,6 +5,14 @@ const PORT = 3002;
 app.use(express.json());
 
 app.use((req, res, next)=>{
+    const authHeader = req.header('auth');
+
+    if(authHeader !== 'secret'){
+        return res.status(401).json({error: "Unauthorized - missing or invalid auth token"});
+    }
+    next();
+});
+app.use((req, res, next)=>{
     console.log(`${req.method} ${req.url}`);
     next();
 });
