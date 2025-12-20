492ff3f1db5637f11d2ee77d3fcba7be01c6f7ff
Amanuel Solomon <amanuel.ugr-0540-16@aau.edu.et>
2025-12-20 18:24:53 +0300

    Portion 2: Front-end setup with React and basic routing.

--- todo-json-server/frontend/src/App.js
+++ todo-json-server/frontend/src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import {Routes, Route, Link} from 'react-router-dom';
+function Home(){
+  return <h1>Home</h1>;
+}
+function Tasks(){
+  return <h1>Tasks</h1>
+}
+function About(){
+  return <h1>About</h1>
+}
 function App(){
   return (
     <div>
-      <h1>To-Do App</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/tasks">Tasks</Link>
+        <Link to="/About">About</Link>
+      </nav>
+      <Routes>
+        <Route Path="/" element={<Home/>}/>
+        <Route path="/taska" element={<Tasks />}/>
+        <Route Path="/About" element={<About />}/>
+      </Routes>
     </div>
   );
 }
